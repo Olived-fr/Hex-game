067df1096b87bbf565afc14943892810f70f8823
hexdu66 <Arnaud@MacBook-Pro-de-Arnaud-2.local>
2016-03-21 14:52:18 +0100

    Choix
    Choisir son adversaire ainsi que sa couleur

---
+++ Rules/rules.c
@@ -0,0 +1,21 @@
+int choix_du_joueur()
+{
+	if(/* condition */)
+	{
+		return 1;
+	}
+	else if (/* condition */)
+	{
+		return 2;
+	}
+	else
+	{
+		return 3;
+	}
+	return ;
+}
+
+bool premier_joueur()
+{
+	return ;
+}
---
+++ Rules/rules.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+int choix_du_joueur();
+bool premier_joueur();
