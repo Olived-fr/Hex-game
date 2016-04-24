068f079254c841fbcae9611320b311728ace726e
hexdu66 <arnaud.soler66@gmail.com>
2016-04-24 17:28:46 +0200

    Suppression des doublons

--- Regles/regles.c
+++ Regles/regles.c
@@ -190,36 +190,3 @@ bool coup_valide(Plateau p,Coordonnees_tab c)
 	return dans_plateau(c) && p[c.abscisse][c.ordonnee].coul==neutre;
 }
 
-
-
-Joueur changer_joueur(Joueur joueur_courant) 
-{
-	if (joueur_courant==joueur1)
-		return joueur2;
-	else
-		return joueur1;
-}
-
-
-Couleur couleur_joueur(Joueur j)
-{
-	if(j==joueur1)
-		return rouge;
-	else
-		return bleu;
-}
-
-
-
-Plateau choix_coup(Plateau p,Coup c)
-{
-	if(coup_valide(p,c.coord))
-		p[c.coord.abscisse][c.coord.ordonnee].coul=c.couleur;
-	return p;
-}
-
-bool coup_valide(Plateau p,Coordonnees_tab c)
-{
-	return dans_plateau(c) && p[c.abscisse][c.ordonnee].coul==neutre;
-}
-
