b00262768ecd5c7b03b28c474cbaf3ce6a6aaaa3
hexdu66 <arnaud.soler66@gmail.com>
2016-04-24 17:01:47 +0200

    Verification du vainqueur
    Ajout des fonctions bord, bord_oppose, verify_win

--- Regles/regles.c
+++ Regles/regles.c
@@ -1,35 +1,197 @@
-#include "rules.h"
+#include "regles.h"
 
 bool premier_joueur(Joueur choix_premier)
 {
 	return choix_premier==joueur1;
 }
 
-void nouvelle_partie(Plateau p, int choix)
+Plateau nouvelle_partie()
 {
-	switch (choix)
-	{
-		case 1:
-			p=initialiser_plateau();
-			creation_config();
-		break;
+	Plateau p;
+	p=initialiser_plateau();
+	creation_config();
 
-		case 2:
-			p=initialiser_plateau();
-			creation_config();
-		break;
-
-		case 3:
-			p=initialiser_plateau();
-			creation_config();
-		break;
-
-		default:
-			exit(1);
-		break;
-	}
-	
+	return p;
 }
+
+bool bord(Type_Case c)
+/* On vérifie si la case est un bord */
+{
+	bool b=false;
+
+	if(c.coul=bleu)
+	/* Si la case est bleu alors si c'est un bord ses voisins NORD-OUEST et NORD-EST OU ses voisins SUD-OUEST et SUD-EST seront égal à NULL */
+	{
+		if((c.NO==NULL && c.NE==NULL) || (c.SO==NULL && c.SE==NULL))
+		{
+			b=true;
+		}
+	}
+	else
+	/* Sinon la case est rouge, si c'est un bord ses voisins OUEST et SUD-OUEST OU ses voisins NORD-EST et EST seront égal à NULL */
+	{
+		if((c.O==NULL && c.SE==NULL) || (c.NE==NULL && c.E==NULL))
+		{
+			b=true;
+		}
+	}
+	return b;
+}
+
+bool bord_oppose(Type_Case deb, Type_Case comp)
+/* Retourne vrai si la case comp (comparaison) est le bord opposé de la case deb (début) */
+{
+	if(deb.NE==NULL && deb.E==NULL)
+	/* Compare case bord droit à case bord gauche */
+	{
+		return (comp.O==NULL && comp.SO==NULL);
+	}
+	else if(deb.O==NULL && deb.SO==NULL)
+	/* Compare case bord gauche à case bord droit */
+	{
+		return (comp.NE==NULL && comp.E==NULL);
+	}
+	else if(deb.NO==NULL && deb.NE==NULL)
+	/* Compare case bord haut à case bord bas */
+	{
+		return (comp.SO==NULL && comp.SE==NULL);
+	}
+	else if(deb.SO==NULL && deb.SE==NULL)
+	/* Compare case bord bas à case bord haut */
+	{
+		return (comp.NO==NULL && comp.NE==NULL);
+	}
+
+}
+
+bool verify_win(Type_Case cur, Type_Case prev, Type_Case deb)
+/* Vérification de la victoire d'un des deux joueurs */
+{
+	int i=0;
+
+	while(!bord_oppose(deb,cur) && i!=6)
+	/* On continue tant que tous les voisins n'ont pas été checkés et tant que le bord opposé n'a pas été trouvé */
+	{
+		switch (i)
+		{
+			case 0:
+				else if(cur.SE==prev || cur.SE->coul!=cur.coul || cur.SE==NULL)
+				{
+					i++;
+				}
+				else
+				{
+					verify_win(cur.SE,cur);
+				}
+			break;
+
+			case 1:
+				if(cur.SO==prev || cur.SO->coul!=cur.coul || cur.SO==NULL)
+				{
+					i++;
+				}
+				else
+				{
+					verify_win(cur.SO,cur);
+				}
+			break;
+
+			case 2:
+				if(cur.O==prev || cur.O->coul!=cur.coul || cur.O==NULL)
+				{
+					i++;
+				}
+				else
+				{
+					verify_win(cur.O,cur);
+				}
+			break;
+
+			case 3:
+				if(cur.NO==prev || cur.NO->coul!=cur.coul || cur.NO==NULL)
+				{
+					i++;
+				}
+				else
+				{
+					verify_win(cur.NO,cur);
+				}
+			break;
+
+			case 4:
+				if(cur.NE==prev || cur.NE->coul!=cur.coul || cur.NE==NULL)
+				{
+					i++;
+				}
+				else
+				{
+					verify_win(cur.NE,cur);
+				}
+			break;
+
+			default:
+				if(cur.E==prev || cur.E->coul!=cur.coul || cur.E==NULL)
+				{
+					i++;
+				}
+				else
+				{
+					verify_win(cur.E,cur);
+				}
+			break;
+		}
+	}
+
+	if(i==6)
+	/* Si la boucle s'est arrêté en ayant incrémenté i jusqu'à 6 alors elle n'a pas détecté de bord */
+	{
+		return false;
+	}
+	else
+	{
+		return true;
+	}
+}
+
+<<<<<<< HEAD
+int main()
+{
+
+    return 0;
+}
+=======
+
+Joueur changer_joueur(Joueur joueur_courant) 
+{
+	if (joueur_courant==joueur1)
+		return joueur2;
+	else
+		return joueur1;
+}
+
+
+Couleur couleur_joueur(Joueur j)
+{
+	if(j==joueur1)
+		return rouge;
+	else
+		return bleu;
+}
+
+
+
+Plateau choix_coup(Plateau p,Coup c)
+{
+	if(coup_valide(p,c.coord))
+		p[c.coord.abscisse][c.coord.ordonnee].coul=c.couleur;
+	return p;
+}
+
+bool coup_valide(Plateau p,Coordonnees_tab c)
+{
+	return dans_plateau(c) && p[c.abscisse][c.ordonnee].coul==neutre;
+}
+<<<<<<< Updated upstream
 
 
 Joueur changer_joueur(Joueur joueur_courant) 
@@ -62,3 +224,6 @@ bool coup_valide(Plateau p,Coordonnees_tab c)
 {
 	return dans_plateau(c) && p[c.abscisse][c.ordonnee].coul==neutre;
 }
+=======
+>>>>>>> origin/master
+>>>>>>> Stashed changes
--- Regles/regles.h
+++ Regles/regles.h
@@ -5,15 +5,34 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
+<<<<<<< Updated upstream
 #include "../Structure/plateau.h"
+=======
+<<<<<<< HEAD
+#include "plateau.h"
+=======
+#include "../Structure/plateau.h"
+>>>>>>> origin/master
+>>>>>>> Stashed changes
 #include "../Persistance/persistance.h"
 
-int choix_du_joueur();
 bool premier_joueur();
+<<<<<<< HEAD
+PLateau nouvelle_partie();
+bool bord(Type_Case c);
+bool bord_oppose(Type_Case deb, Type_Case comp);
+bool verify_win(Type_Case cur, Type_Case prev, Type_Case deb);
+
+=======
 void nouvelle_partie(Plateau p);
 Joueur changer_joueur(Joueur joueur_courant); //renvoie "joueur1" si joueur_courant==joueur2 et renvoie "joueur2" si joueur_courant==joueur1
 Couleur couleur_joueur(Joueur j);//renvoie "bleu" si j est égal à "joueur1" et "rouge si j est égal à "joueur2"
 Plateau choix_coup(Plateau p,Coup c);/* Mets à jour un tableau en fonction d'un coup c après avoir vérifie la validité de ce dernier*/
 bool coup_valide(Plateau p,Coordonnees_tab c); // renvoie true si la case de coordonnees c est libre et est dans le plateau
 
+<<<<<<< Updated upstream
 #endif
+=======
+#endif
+>>>>>>> origin/master
+>>>>>>> Stashed changes
