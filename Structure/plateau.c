14446a8ae48b4102c349d32798437fafcf5cbbf9
Rialy <rialy.andriamiseza@yahoo.fr>
2016-04-28 13:11:32 +0200

    Correction initialisation
    Inversion du sens d'inclinaison du plateau dans l'initialisation

--- Structure/plateau.c
+++ Structure/plateau.c
@@ -13,73 +13,73 @@ void initialiser_plateau(Plateau p)
 			switch(y)
 			{
 				case 0 : //si la case courante est à l'extrémité ouest
+					p[x][y].SO=NULL;//on ne lui affecte pas de voisin sud ouest
 					p[x][y].O=NULL;//on ne lui affecte pas de voisin ouest
-					p[x][y].NO=NULL;//on ne lui affecte pas de voisin nord ouest
-					p[x][y].E=&p[x][y+1];//on lui affecte la case à sa droite comme voisin de droite
+					p[x][y].E=&p[x+1][y];//elle a forcément un voisin est
 					switch(x)
 					{
 						case 0 : //si la case courante est à l'extrémité nord
-							p[x][y].NE=NULL;//on ne lui affecte pas de voisin nord est
-							p[x][y].SE=&p[x+1][y+1];
-							p[x][y].SO=&p[x+1][y];
+							p[x][y].NO=NULL;//on ne lui affecte pas de voisin nord est
+							p[x][y].NE=NULL;//on ne lui affecte pas de voisin nord ouest
+							p[x][y].SE=&p[x][y+1];
 							break;
 						case LIGNE_MAX-1 ://si la case courante est à l'extrémité sud
-							p[x][y].NE=&p[x-1][y];
+							p[x][y].NO=&p[x][y-1];
+							p[x][y].NE=&p[x+1][y-1];
 							p[x][y].SE=NULL;//on ne lui affecte pas de voisin sud est
-							p[x][y].SO=NULL;//on ne lui affecte pas de voisin sud est
 							break;
 						default :
-							p[x][y].NE=&p[x-1][y];
-							p[x][y].SE=&p[x+1][y+1];
-							p[x][y].SO=&p[x+1][y];
+							p[x][y].NO=&p[x][y-1];
+							p[x][y].NE=&p[x+1][y-1];
+							p[x][y].SE=&p[x][y+1];
 							break;
 					}
 					break;
 				case COLONNE_MAX-1 : //si la case courante est à l'extrémité est
 					p[x][y].E=NULL;//on ne lui affecte pas de voisin est
-					p[x][y].SE=NULL;//on ne lui affecte pas de voisin sud est
-					p[x][y].O=&p[x][y-1];//on lui affecte la case à sa gauche comme voisin de gauche
+					p[x][y].NE=NULL;//on ne lui affecte pas de voisin nord est
+					p[x][y].O=&p[x-1][y];//elle a forcément un voisin ouest
 					switch(x)
 					{
 						case 0 : //si la case courante est à l'extrémité nord
-							p[x][y].NE=NULL;//on ne lui affecte pas de voisin nord ouest
+							p[x][y].SE=&p[x][y+1];
 							p[x][y].NO=NULL;//on ne lui affecte pas de voisin nord ouest
-							p[x][y].SO=&p[x+1][y];
+							p[x][y].SO=&p[x-1][y+1];
 							break;
 						case LIGNE_MAX-1 : //si la case courante est à l'extrémité sud
-							p[x][y].NE=&p[x-1][y];
-							p[x][y].NO=&p[x-1][y-1];
-							p[x][y].SO=NULL;//on ne lui affecte pas de voisin sudd ouest
+							p[x][y].SE=&p[x][y+1];
+							p[x][y].NO=&p[x][y-1];
+							p[x][y].SO=&p[x-1][y+1];;
 							break;
 						default :
-							p[x][y].NE=&p[x-1][y];
-							p[x][y].NO=&p[x-1][y-1];
-							p[x][y].SO=&p[x+1][y];
+							p[x][y].SE=NULL;//on ne lui affecte pas de voisin sud est
+							p[x][y].NO=&p[x][y-1];
+							p[x][y].SO=NULL;//on ne lui affecte pas de voisin sud ouest
 							break;
 					}
 					break;
 				default : //sinon
-					p[x][y].E=&p[x][y+1];//on ne lui affecte pas de voisin est
-					p[x][y].O=&p[x][y-1];//on lui affecte la case à sa gauche comme voisin de gauche
+					p[x][y].E=&p[x+1][y];
+					p[x][y].O=&p[x-1][y];
 					switch(x)
 					{
 						case 0 : //si la case courante est à l'extrémité nord
 							p[x][y].NO=NULL;//on ne lui affecte pas de voisin nord ouest
 							p[x][y].NE=NULL;//on ne lui affecte pas de voisin nord est
-							p[x][y].SO=&p[x+1][y];
-							p[x][y].SE=&p[x+1][y+1];
+							p[x][y].SO=&p[x-1][y+1];
+							p[x][y].SE=&p[x][y+1];
 							break;
 						case LIGNE_MAX-1 : //si la case courante est à l'extrémité sud
-							p[x][y].NO=&p[x-1][y-1];
-							p[x][y].NE=&p[x-1][y];
+							p[x][y].NO=&p[x][y-1];
+							p[x][y].NE=&p[x+1][y-1];
 							p[x][y].SO=NULL;//on ne lui affecte pas de voisin sud ouest
 							p[x][y].SE=NULL;//on ne lui affecte pas de voisin sud est
 							break;
 						default :
-							p[x][y].NO=&p[x-1][y-1];
-							p[x][y].NE=&p[x-1][y];
-							p[x][y].SO=&p[x+1][y];
-							p[x][y].SE=&p[x+1][y+1];
+							p[x][y].NO=&p[x][y-1];
+							p[x][y].NE=&p[x+1][y-1];
+							p[x][y].SO=&p[x-1][y+1];
+							p[x][y].SE=&p[x][y+1];
 							break;
 					}
 					break;
