3f7e8f04ba6db8b54cf690e5c95f35df44f46833
Rialy <rialy.andriamiseza@yahoo.fr>
2016-05-07 15:11:48 +0200

    DotA > LoL

--- IA/IA1.c
+++ IA/IA1.c
@@ -7,7 +7,7 @@ Coordonnees_tab coup_IA1(Plateau p)
 	bool trouve=false;
 	Type_Case* case_courante;
 	Type_Case* case_proche=&p[5][0];
-	if(case_proche->coul!=neutre)case_proche=case_proche.O;
+	if(case_proche->coul!=neutre)case_proche=case_proche->O;
 	//si le plateau est de taille 11 par exemple la distance maximale qu'une case peut avoir à un bord est égal à 10
 	for(x=0;x<LIGNE_MAX;x++)
 	{
@@ -15,11 +15,11 @@ Coordonnees_tab coup_IA1(Plateau p)
 		{
 			distance_min=LIGNE_MAX+2;
 			case_courante=&p[x][y];
-			if(case_courante.coul==rouge)
+			if(case_courante->coul==rouge)
 			{
 				trouve=true;
 				reinitialise_case_checked(p);
-				d=distance_bord_sud(p,case_courante,case_courante.SE);
+				d=distance_bord_sud(p,case_courante,case_courante->SE);
 				if(d < distance_min && d!=0)
 				{
 					case_proche=case_courante;
@@ -34,9 +34,9 @@ Coordonnees_tab coup_IA1(Plateau p)
 	if(trouve)
 	{
 		reinitialise_case_checked(p);
-		case_proche=*contourner(&case_proche,case_proche.SE);
+		case_proche=*contourner(&case_proche,case_proche->SE);
 	}
-	return case_proche.co;
+	return case_proche->co;
 }
 
 int distance_bord_sud(Plateau p,Type_Case* c,Type_Case* voisin)
@@ -47,13 +47,12 @@ int distance_bord_sud(Plateau p,Type_Case* c,Type_Case* voisin)
 		return 0;
 	else
 	{
-		voisin->check=true;
 		Type_Case* nouveau_voisin=contourner(c,voisin);
 		if(i==6 || nouveau_voisin==NULL)
 			return LIGNE_MAX+1;
 		//cas de l'impasse
 		else
-			return(1+distance_bord_sud(p,*nouveau_voisin,nouveau_voisin->SE));
+			return(1+distance_bord_sud(p,nouveau_voisin,nouveau_voisin->SE));
 	}
 }
 
@@ -63,6 +62,7 @@ Type_Case* contourner(Type_Case* case_choisie,Type_Case* voisin)
 	int i=0;
 	while(i!=6 && (temp==NULL || temp->coul!=neutre || temp->check))
 	{
+		temp->check=true;
 		i++;
 		temp=voisin_suivant(case_choisie,temp);
 	}
