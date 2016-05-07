a0776f0b525dbc8d897898abf6fa401b559d9b17
Rialy <rialy.andriamiseza@yahoo.fr>
2016-05-07 22:35:39 +0200

    Revert "I"
    This reverts commit 0a3b0ef2fdc9c14d9c51ca8cd9d3286e4483c7a5.

--- IA/IA1.c
+++ IA/IA1.c
@@ -6,13 +6,12 @@ Coordonnees_tab coup_IA1(Plateau p)
 	int depart=rand()%11;
 	int x,y;
 	int distance_min,nb_appels;
-	int min_courant=LIGNE_MAX+2;
+	int min_interne=LIGNE_MAX+2;
 	bool trouve=false;
 	Type_Case* case_courante;
 	Type_Case* case_proche=&p[depart][0];
 	distance_min=COLONNE_MAX+2;
-	if(case_proche->coul!=neutre)
-		case_proche=case_proche->O;
+	if(case_proche->coul!=neutre)case_proche=case_proche->O;
 	//si le plateau est de taille 11 par exemple la distance maximale qu'une case peut avoir à un bord est égal à 10
 	for(y=0;y<LIGNE_MAX;y++)
 	{
@@ -24,11 +23,11 @@ Coordonnees_tab coup_IA1(Plateau p)
 				trouve=true;
 				reinitialise_case_checked(p);
 				nb_appels=0;
-				d=distance_bord_sud(case_courante,&min_courant);
-				if(d <= distance_min && d!=0)
+				distance_bord_sud(case_courante,case_courante->SE,&nb_appels,&min_interne);
+				if(nb_appels <= distance_min && nb_appels!=0)
 				{
 					case_proche=case_courante;
-					distance_min=d;
+					distance_min=nb_appels;
 				}
 				/*Ainsi, on retient la distance la plus courte et on retient également la direction la plus courte avec la variable "distance_choisie"*/
 			}
@@ -44,26 +43,30 @@ Coordonnees_tab coup_IA1(Plateau p)
 	return case_proche->co;
 }
 
-int distance_bord_sud(Type_Case* c,int* min)
-{	
-	int i=0;
-	int distance=0;
-	Type_Case* voisin=c->SE;
+void distance_bord_sud(Type_Case* c,Type_Case* voisin,int* d,int* min_interne)
+{
+	
+	int temp,i;
 	c->check=true;
-	if(c->SE!=NULL)
+	if(voisin!=NULL)
 	{
+		*d=*d+1;
+		Type_Case* nouveau_voisin=voisin;
+		i=0;
 		while(i!=6)
 		{
-			if(voisin!=NULL && voisin->coul!=bleu && !voisin->check)
+			if(nouveau_voisin!=NULL && !nouveau_voisin->check && nouveau_voisin->coul!=bleu)
 			{
-				distance=1+distance_bord_sud(voisin,min);
-				if(distance < *min)
-					*min=distance;
+				temp=*d;
+				distance_bord_sud(nouveau_voisin,nouveau_voisin->SE,&temp,min_interne);
+				if(temp < *min_interne)
+					*min_interne=temp;
 			}
-			voisin=voisin_suivant(c,voisin);
+			i++;
+			nouveau_voisin=voisin_suivant(c,nouveau_voisin);
 		}
+		*d=*min_interne;
 	}
-	return distance;
 }
 
 Type_Case* contourner(Type_Case* case_choisie,Type_Case* voisin)
