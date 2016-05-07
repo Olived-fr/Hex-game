fe4ec1d0e8f65bc39329f034a22c5e1250c95667
Rialy <rialy.andriamiseza@yahoo.fr>
2016-05-07 16:43:58 +0200

    xxxx

--- IA/IA1.c
+++ IA/IA1.c
@@ -4,7 +4,7 @@ Coordonnees_tab coup_IA1(Plateau p)
 {
 	int x,y,d;
 	int distance_min;
-	int dis;
+	int nb_appel;
 	bool trouve=false;
 	Type_Case* case_courante;
 	Type_Case* case_proche=&p[5][0];
@@ -20,12 +20,12 @@ Coordonnees_tab coup_IA1(Plateau p)
 			{
 				trouve=true;
 				reinitialise_case_checked(p);
-				dis=0;
-				d=distance_bord_sud(p,case_courante,case_courante->SE,&dis);
-				if(d <= distance_min && d!=0)
+				nb_appel=0;
+				distance_bord_sud(p,case_courante,case_courante->SE,&nb_appel);
+				if(nb_appel <= distance_min && nb_appel!=0)
 				{
 					case_proche=case_courante;
-					distance_min=d;
+					distance_min=nb_appel;
 				}
 				/*Ainsi, on retient la distance la plus courte et on retient également la direction la plus courte avec la variable "distance_choisie"*/
 			}
@@ -43,9 +43,8 @@ Coordonnees_tab coup_IA1(Plateau p)
 
 int distance_bord_sud(Plateau p,Type_Case* c,Type_Case* voisin,int* d)
 {
-	d++;
-	int temp=*d;
-	int i;
+	*d=*d+1;
+	int temp,i;
 	int min_interne=LIGNE_MAX+2;
 	c->check=true;
 	if(voisin==NULL)
@@ -53,20 +52,20 @@ int distance_bord_sud(Plateau p,Type_Case* c,Type_Case* voisin,int* d)
 	else
 	{
 		Type_Case* nouveau_voisin=voisin;
-		int i=0;
+		i=0;
 		while(i!=6)
 		{
 			if(nouveau_voisin!=NULL && !nouveau_voisin->check && nouveau_voisin->coul==neutre)
 			{
 				temp=*d;
-				distance_bord_sud(p,nouveau_voisin,nouveau_voisin->SE,temp);
+				distance_bord_sud(p,nouveau_voisin,nouveau_voisin->SE,&temp);
 				if(nouveau_voisin->coul==neutre && temp < min_interne)
 					min_interne=temp;
 			}
 			i++;
 			nouveau_voisin=voisin_suivant(c,nouveau_voisin);
 		}
-		return d;
+		return min_interne;
 	}
 }
 
